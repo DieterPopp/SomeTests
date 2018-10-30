e8ed61289abddae246c9f0399aa63ea66a87a507
DieterPopp <Dieter.Popp@ednc-solutions.de>
2018-10-30 16:22:28 +0100

    Erster Test GitHub.

---
+++ TestConsoleApp1/TestConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test test = new Test();
+            test.TestMethod();
+        }
+        
+    }
+
+    public class Test
+    {
+        public Test()
+        {
+
+        }
+
+        public void TestMethod()
+        {
+            Console.WriteLine("Das ist ein Test!");
+        }
+    }
+}
